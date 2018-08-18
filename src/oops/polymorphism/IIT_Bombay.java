398c261719b130478a202e09500a0a30524ccf22
rajeshbajya1993 <40539653+rajeshbajya1993@users.noreply.github.com>
2018-08-18 15:32:06 +0530

    Initial commit

---
+++ src/oops/polymorphism/IIT_Bombay.java
@@ -0,0 +1,10 @@
+package oops.polymorphism;
+
+public class IIT_Bombay extends IIT{
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/oops/polymorphism/IIT_Guwahati.java
@@ -0,0 +1,11 @@
+package oops.polymorphism;
+
+public class IIT_Guwahati extends IIT{
+
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
